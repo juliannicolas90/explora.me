9828b9daa048cbb166f062155a44887f52184b92
Julian <julian_nicolas90@hotmail.com>
2018-11-26 22:10:15 -0500

    Cleaning..

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ import dash
 from dash.dependencies import Input, Output, State
 import dash_core_components as dcc
 import dash_html_components as html
-import dash_table_experiments as dt
 
 import dash_table
 
